8f47e49d27f5ef2553959dd06a54737e1e7b5a9a
mumarse~ <umarkhan7672@gmail.com~>
2023-02-03 20:15:32 +0500

    assignment1

--- Assignment # 2/greet.js
+++
@@ -1,7 +0,0 @@
-/* Write an arrow function expression called greet(). It should accept a single argument representing a 
-person's name. It should return a greeting string. */
-
-const greet = (name) => {
-    return `Hello ${name}`;
-}
-console.log(greet("Muhammad Umar"))
